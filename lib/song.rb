0555d0dcd72006560b67fa5bdb559d031753081b
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-22 20:29:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,10 @@ require 'pry'
 
 class Song
   
-  extend Memorable
   extend Findable
   include Paramable
-  
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
